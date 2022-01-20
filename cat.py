251875b01b1bea62cc9156d238b69a6da1ad8818
AnnabellePlourd <95492246+AnnabellePlourde@users.noreply.github.com>
2022-01-20 15:54:04 +0000

    Initial commit

---
+++ cat.py
@@ -0,0 +1,20 @@
+class  cat:
+
+  
+  def __init__(self,_colorOfEyes,_breed,_furColor):
+    self.colorOfEyes=_colorOfEyes
+    self.breed=_breed
+    self.furColor=_furColor
+
+  def play(self):
+    print("scratch")
+
+  def meow(self):
+   print("meow")
+
+  def eat(self):
+     print ("chomp")
+
+     #play
+     #meow
+     #eat
---
+++ main.py
@@ -0,0 +1,7 @@
+class  cat:
+
+  
+  def __init__(self,_colorOfEyes,_breed,_furColor):
+    self.colorOfEyes=_colorOfEyes
+    self.breed=_breed
+    self.furColor=_furColor
